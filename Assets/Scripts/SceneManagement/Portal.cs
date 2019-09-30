9fc6b73a8c33689158e6f88bae0606b709cd8ade
pankow602 <zzrhydonzz1@gmail.com>
2019-09-29 20:26:34 -0400

    S7L69

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Portal : MonoBehaviour
+namespace RPG.SceneManagement
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Portal : MonoBehaviour
     {
-        
-    }
+        [SerializeField] int sceneToLoad = -1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
     }
 }
